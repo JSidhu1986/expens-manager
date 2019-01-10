d84576794b55629c7eb2a6ca0adff4ff6043076e
Jagpreet Sidhu <jagpreetsidhu@Jagpreets-MacBook-Pro.local>
2019-01-09 21:36:28 -0800

    Removing unwanted code

--- src/app.js
+++ src/app.js
@@ -12,20 +12,6 @@ import 'react-dates/lib/css/_datepicker.css';
 
 const store = configureStore();
 
-store.dispatch(addExpense({description: 'Water Bill', amount: 100, createdAt: 10000}));
-store.dispatch(addExpense({description: 'Gas Bill', amount: 300, createdAt: 1001}));
-store.dispatch(addExpense({description: 'Rent', amount: 3000, createdAt: 1041}));
-
-// store.dispatch(removeExpense(expenseTwo.expense.id));
-// store.dispatch(editExpense(expenseOne.expense.id, {amount: 300}));
-
-//store.dispatch(setTextFilter('Water'));
-//store.dispatch(sortByAmount());
-//store.dispatch(sortByDate());
-
-//store.dispatch(setStartDate(1250));
-//store.dispatch(setEndDate(1250));
-
 const jsx = (
     <Provider store={store}>
         <AppRouter />
